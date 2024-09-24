25fbba04d6a5cf10f49209a1e4c5d1524a2476ed
juliarta99 <adij4255@gmail.com>
2024-09-25 01:16:43 +0800

    design homepage, learnings and pricing

--- js/script.js
+++ js/script.js
@@ -6,3 +6,47 @@ $(document).ready(function() {
         $('#image-display').attr('src', imageSrc);
     });
 });
+
+var swiper = new Swiper(".swiper-menu", {
+    slidesPerView: 5,
+    spaceBetween: 10,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".swiper-learnings", {
+    slidesPerView: 4,
+    spaceBetween: 0,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".swiper-menu-testimoni", {
+    // slidesPerView: 1,
+    autoplay: {
+        delay: 5000,
+    },
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    speed: 1000
+});
+
+var swiper = new Swiper(".swiper-menu-learning", {
+    slidesPerView: 7,
+    spaceBetween: 10,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
